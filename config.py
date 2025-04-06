75d99b2e1a7751f008ac75374ebfca796908dc6f
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2025-04-06 12:24:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+USER_SESSION = os.getenv("USER_SESSION")
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+TELEGRAPH_PHOTO = os.getenv("TELEGRAPH_PHOTO")
+OWNER_LINK = os.getenv("OWNER_LINK")
+SUPPORT_LINK = os.getenv("SUPPORT_LINK")
